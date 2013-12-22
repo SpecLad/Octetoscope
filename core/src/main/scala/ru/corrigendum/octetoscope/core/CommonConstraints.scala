933361319667b7bc78270e405bc92e18a7585332
Роман Донченко <dpb@corrigendum.ru>
2013-12-22 04:05:48 +0400

    Added some contraints that are likely to be commonly encountered.

---
+++ core/src/main/scala/ru/corrigendum/octetoscope/core/CommonConstraints.scala
@@ -0,0 +1,33 @@
+/*
+  This file is part of Octetoscope.
+  Copyright (C) 2013 Octetoscope contributors (see /AUTHORS.txt)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package ru.corrigendum.octetoscope.core
+
+private object CommonConstraints {
+  def nonNegative[T](implicit arithm: Numeric[T]) = new ShouldMustConstraint[T] {
+    override def check(value: T): Boolean = arithm.signum(value) >= 0
+    def shouldNote: String = "should be non-negative"
+    def mustNote: String = "must be non-negative"
+  }
+
+  def positive[T](implicit arithm: Numeric[T]) = new ShouldMustConstraint[T] {
+    override def check(value: T): Boolean = arithm.signum(value) > 0
+    def shouldNote: String = "should be positive"
+    def mustNote: String = "must be positive"
+  }
+}
---
+++ core/src/test/scala/ru/corrigendum/octetoscope/core/CommonConstraintsSuite.scala
@@ -0,0 +1,36 @@
+/*
+  This file is part of Octetoscope.
+  Copyright (C) 2013 Octetoscope contributors (see /AUTHORS.txt)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package ru.corrigendum.octetoscope.core
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers._
+
+class CommonConstraintsSuite extends FunSuite {
+  test("nonNegative") {
+    CommonConstraints.nonNegative[Double].check(5.0) should equal (true)
+    CommonConstraints.nonNegative[Int].check(0) should equal (true)
+    CommonConstraints.nonNegative[Long].check(-1L) should equal (false)
+  }
+
+  test("positive") {
+    CommonConstraints.positive[Short].check(4) should equal (true)
+    CommonConstraints.positive[Byte].check(0) should equal (false)
+    CommonConstraints.positive[Float].check(-2.0f) should equal (false)
+  }
+}

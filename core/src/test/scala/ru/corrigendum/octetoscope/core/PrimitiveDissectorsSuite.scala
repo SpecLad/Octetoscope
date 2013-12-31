c70de154a67e63d809cdb39fb8535afc98068351
Роман Донченко <dpb@corrigendum.ru>
2013-12-31 20:36:20 +0400

    Added tests for dissecting empty strings.

--- core/src/test/scala/ru/corrigendum/octetoscope/core/PrimitiveDissectorsSuite.scala
+++ core/src/test/scala/ru/corrigendum/octetoscope/core/PrimitiveDissectorsSuite.scala
@@ -35,12 +35,14 @@ class PrimitiveDissectorsSuite extends FunSuite {
   }
 
   test("asciiString") {
+    verify(asciiString(0), "\"\"", "")
     verify(asciiString(4), "\"abcd\"", "abcd", 'a', 'b', 'c', 'd')
   }
 
   test("asciiZString") {
     verify(asciiZString(4), "\"abc\"", "abc", 'a', 'b', 'c', 0)
     verify(asciiZString(4), "\"ab\"", "ab", 'a', 'b', 0, 'd')
+    verifyBad(asciiZString(0), "\"\"", "")
     verifyBad(asciiZString(4), "\"abcd\"", "abcd", 'a', 'b', 'c', 'd')
   }
 

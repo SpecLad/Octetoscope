85a7025cf6f51177f7f9c1f825b09eb354250524
Роман Донченко <dpb@corrigendum.ru>
2015-07-13 00:17:27 +0300

    Remove unneeded import

--- buildSrc/src/main/groovy/GenerateFileTask.groovy
+++ buildSrc/src/main/groovy/GenerateFileTask.groovy
@@ -18,7 +18,6 @@
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
